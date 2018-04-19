c7467991a9995d79be9a54e4aeaa5aca062b99be
Mohammad <mohammad8758@hotmail.com>
2018-04-19 10:25:00 +0530

    Initial Commit

---
+++ server.js
@@ -0,0 +1,89 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+var app = express();
+
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'hbs');
+
+app.use((req, res, next) => {
+    var now = new Date().toString();
+    var log = `${now}: ${req.method} ${req.url}`;
+    
+    console.log(log);
+    
+    fs.appendFile('server.log', log, (err) => {
+        if(err) {
+            console.log('Unable to append log to file');
+        }
+    });
+    next();
+});
+app.use((req, res, next) => {
+    res.render('maintenance.hbs', {
+               pageTitle: 'The site is currently under MAINTENANCE!!!'
+               })
+})
+
+
+hbs.registerHelper('getCurrentYear', () => {
+    return new Date().getFullYear();
+})
+
+app.use(express.static(__dirname + '/public'));
+app.get('/', (req, res) => {
+   res.render('home.hbs', {
+       pageTitle: 'Home Page',
+       message: 'Welcome to this site'
+   });
+});
+
+app.get('/about', (req, res) => {
+    res.render('about.hbs', {
+        pageTitle: 'About Page'
+    });
+});
+
+
+
+app.listen(3000, () => {
+    console.log('Listening to the portaaaa');
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
